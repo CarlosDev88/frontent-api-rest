f110d66dfd7015f8e8fe2d3f54460a2d15944736
Carlos <arturorueda88@gmail.com>
2022-10-07 13:43:11 -0500

    cambios en axios.config

--- src/helpers/axios-config.js
+++ src/helpers/axios-config.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:9000/",
+  // baseURL: "http://localhost:9000/",
+  baseURL: "https://backend-node-carlos-carlosdev88.vercel.app/",
 });
 
 export { axiosInstance };
